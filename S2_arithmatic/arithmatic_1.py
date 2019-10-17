00b64b50cb404c8e1e4d110cc93ca865e25b4196
Anubhav311 <anubhavbhambri1@gmail.com>
2019-10-17 16:58:56 +0530

    completed arithmatic_1.py

--- S2_arithmatic/arithmatic_1.py
+++ S2_arithmatic/arithmatic_1.py
@@ -1,4 +1,5 @@
 #Guided project
+print("Guided project solutions:")
 print(f'4 + 8 = {4 + 8}') # => 12
 
 print(f'4 + 1 + 5 = {4 + 1 + 5}') # => 10
@@ -15,3 +16,7 @@ print(f'(1 + 3)/4 = {(1 + 3)/4}') # => 1.0
 
 
 #Exercise solution
+print("\n\nExercise solutions:")
+print(f'5 - 2/4 * 3/5 = {5 - 2/4 * 3/5}') # => 4.7
+
+print(f'(4 - 5) / (3 + 5 * 6) = {(4 - 5) / (3 + 5 * 6)}')
